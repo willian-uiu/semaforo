76a578f446811069604ac3464e161907587eec6f
Willian da Silva Mamede <willianx2002.ws@gmail.com>
2024-08-11 17:45:43 -0300

    closes #3 - Javascript e interação

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function changeTrafficLight() {
+
+    if (document.getElementById("green").className === "light green-light") {
+        document.getElementById("yellow").className = "light yellow-light";
+        document.getElementById("green").className = "light light-off";
+    } else {
+        if (document.getElementById("yellow").className === "light yellow-light"){
+            document.getElementById("red").className = "light red-light";
+            document.getElementById("yellow").className = "light light-off";
+        } else {
+            document.getElementById("green").className = "light green-light";
+            document.getElementById("red").className = "light light-off";
+        }
+    }
+}
